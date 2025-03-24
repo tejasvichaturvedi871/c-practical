4b69abbe876319ee433ed6bc795c3e65d73b67a8
tejasvichaturvedi871 <tejasvichaturvedi777@gmail.com>
2025-03-24 09:49:17 +0530

    Update Practical 10.cpp

--- Practical 10.cpp
+++ Practical 10.cpp
@@ -1,6 +1,7 @@
 //##10  Create a Triangle class. Add exception handling statements to ensure the following conditions: all sides are greater than 0 and sum of any two sides are greater than the third side.The class should also have overloaded functions for calculating the area of a right angled triangle as well as using Heron's formula to calculate the area of any type of triangle.*
 
 #include <iostream> 
+#include <stdexcept>
 using namespace std; 
 
 class Triangle { 
